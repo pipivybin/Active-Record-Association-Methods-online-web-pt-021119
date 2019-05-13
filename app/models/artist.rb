b819c5dc11d7348d8d5592128b30f70bf7aed69a
Ming Duan <11203duan@gmail.com>
2019-05-13 18:55:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,12 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Artist.find(0).genre
+    self.find(0).genre
     #return the genre of the artist's first saved song
   end
 
   def song_count
+    
     #return the number of songs associated with the artist
   end
 
